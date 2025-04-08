44b4815ab5a8586fe9ff58746a022b244b485be1
JuliaPro1 <julpkolp@gmail.com>
2025-04-08 20:22:04 +0300

    Update Main.java

--- src/main/java/com/julpkolp/Main.java
+++ src/main/java/com/julpkolp/Main.java
@@ -2,6 +2,6 @@ package com.julpkolp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, GIT!");
+        System.out.println("Hello, there!");
     }
 }

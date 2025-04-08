7321fe4263ddc2411d67a34abcb5f17d78352f74
Julia <julpkolp@gmail.com>
2025-04-08 21:09:01 +0300

    New message was added

--- src/main/java/com/julpkolp/Main.java
+++ src/main/java/com/julpkolp/Main.java
@@ -2,6 +2,9 @@ package com.julpkolp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("new message!");
+        System.out.println(getMessage());
+    }
+    private static String getMessage() {
+        return "new message!";
     }
 }
